e8d747e7d20c9f0f66d498d0a7a4d4d6f44ea290
frzertade <frezertade@gmail.com>
2021-06-19 17:39:16 -0400

    code changed

--- server.py
+++ server.py
@@ -5,41 +5,43 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-myclient = pymongo.MongoClient(
-    "mongodb://fre:frezer@cluster0-shard-00-00.7xkdf.mongodb.net:27017,cluster0-shard-00-01.7xkdf.mongodb.net:27017,cluster0-shard-00-02.7xkdf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-11kjk0-shard-0&authSource=admin&retryWrites=true&w=majority")
-mydb = myclient["mydatabase"]
-mycol = mydb["News"]
-mycolAbiy = mydb["abiyahmadali"]
-mycolTikvah = mydb["tikvah"]
-mycolBisrat = mydb["BisratSport"]
-mycolEbc = mydb["ebc"]
+# myclient = pymongo.MongoClient(
+#     "mongodb://fre:frezer@cluster0-shard-00-00.7xkdf.mongodb.net:27017,cluster0-shard-00-01.7xkdf.mongodb.net:27017,cluster0-shard-00-02.7xkdf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-11kjk0-shard-0&authSource=admin&retryWrites=true&w=majority")
+# mydb = myclient["mydatabase"]
+# mycol = mydb["News"]
+# mycolAbiy = mydb["abiyahmadali"]
+# mycolTikvah = mydb["tikvah"]
+# mycolBisrat = mydb["BisratSport"]
+# mycolEbc = mydb["ebc"]
+#
+#
+# @app.route('/news', methods=['GET'])
+# def get_all_stars():
+#     return dumps(list(mycol.find().limit(50)), indent=2)
+#
+#
+# @app.route('/news/telegram/tikvah', methods=['GET'])
+# def get_all_telegram_tikvah():
+#     return dumps(list(mycolTikvah.find().limit(20)), indent=2)
+#
+#
+# @app.route('/news/telegram/ebc', methods=['GET'])
+# def get_all_telegram_ebc():
+#     return dumps(list(mycolEbc.find().limit(20)), indent=2)
+#
+#
+# @app.route('/news/telegram/bisratsport', methods=['GET'])
+# def get_all_telegram_bisratsport():
+#     return dumps(list(mycolBisrat.find().limit(20)), indent=2)
+#
+#
+# @app.route('/news/twitter/abiyahmadali', methods=['GET'])
+# def get_all_twitter():
+#     return dumps(list(mycolAbiy.find().limit(20)), indent=2)
 
-
-@app.route('/news', methods=['GET'])
-def get_all_stars():
-    return dumps(list(mycol.find().limit(50)), indent=2)
-
-
-@app.route('/news/telegram/tikvah', methods=['GET'])
-def get_all_telegram_tikvah():
-    return dumps(list(mycolTikvah.find().limit(20)), indent=2)
-
-
-@app.route('/news/telegram/ebc', methods=['GET'])
-def get_all_telegram_ebc():
-    return dumps(list(mycolEbc.find().limit(20)), indent=2)
-
-
-@app.route('/news/telegram/bisratsport', methods=['GET'])
-def get_all_telegram_bisratsport():
-    return dumps(list(mycolBisrat.find().limit(20)), indent=2)
-
-
-@app.route('/news/twitter/abiyahmadali', methods=['GET'])
-def get_all_twitter():
-    return dumps(list(mycolAbiy.find().limit(20)), indent=2)
-
-
+@app.route('/', methods=['GET'])
+def welcome():
+    return "welcome to our api"
 
 if __name__:
     app.run()
